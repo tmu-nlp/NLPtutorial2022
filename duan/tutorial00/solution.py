da8f0d5fe7750cce9ac75ec3136738c60f82d095
DUAN <duanwenjie1994@gmail.com>
2022-04-11 18:14:37 +0900

    TUTORIAL00

---
+++ duan/tutorial00/solution.py
@@ -0,0 +1,10 @@
+# sents=['this is a pen','this pen is my pen']
+sents = open("../../../nlptutorial/data/wiki-en-train.word").readlines()
+d = {}
+for sent in sents:
+    for word in sent.strip().split(' '): 
+        if word not in d.keys():
+            d[word]=0
+        d[word]+=1
+
+print(list(d.items())[:10])
