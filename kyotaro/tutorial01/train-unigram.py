db0385a6941ef364036a81741cc060ac5bf50cf2
tikiti-kyotaro <kyotaro0104@outlook.jp>
2022-04-18 18:53:03 +0900

    done

---
+++ kyotaro/tutorial01/train-unigram.py
@@ -0,0 +1,28 @@
+import sys
+from collections import defaultdict
+
+my_dict = defaultdict(lambda: 0)
+my_file = open(sys.argv[1], "r").readlines()
+p = []
+ans = 1
+count = 0
+
+for line in my_file:
+    line = line.strip()
+    word = line.split()
+    word.append("</s>")
+    #print(len(word))
+    count += len(word)
+    for i in range(len(word)):
+        my_dict[word[i]] += 1
+
+for key, value in sorted(my_dict.items()):
+    p.append(value / count)
+
+for i in range(len(p)):
+    ans *= p[i]
+    
+index = 0
+for key, value in sorted(my_dict.items()):
+    print(key + " " + str('{:.6f}'.format(p[index])))
+    index += 1
