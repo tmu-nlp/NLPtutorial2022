2956b310164ad6a2c3eb58bfde1815acf73a7eee
ayaco0 <zaurusu8569@yahoo.co.jp>
2022-04-25 16:25:33 +0900

    modified

--- ayako/tutorial01/train-unigram.py
+++ ayako/tutorial01/train-unigram.py
@@ -1,9 +1,6 @@
 import sys
 from collections import defaultdict
 
-
-
-
 my_dict = defaultdict(lambda: 0)#デフォルトの値を0にする
 my_file = open(sys.argv[1], "r").readlines()#コマンドラインで指定したファイルを読み込む
 count = 0  
@@ -12,12 +9,8 @@ for line in my_file:
     my_list = line.strip().split()#空白区切りで単語を配列に格納
     my_list.append("</s>")
     for value in my_list:
-        my_dict[value] += 1 #key:word, value:count
+        my_dict[value] += 1
         count += 1 #1単語1カウント
-
-#for key, value in enumerate(my_dict):
-    #p_dict[value] = '{:.6f}'.format(float(key)/count)
-
 
 for key, value in sorted(my_dict.items()):##キー・値の各組をキー順に表示する
     print(key,'{:.6f}'.format(float(value)/count))
