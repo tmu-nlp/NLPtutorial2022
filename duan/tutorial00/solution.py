89c390712fca4e1fef74a2323e9a0fa1c6db3218
DUAN‐2022 <87805036+standalone2021@users.noreply.github.com>
2022-05-02 14:53:17 +0900

    Update solution.py

--- duan/tutorial00/solution.py
+++ duan/tutorial00/solution.py
@@ -1,10 +1,8 @@
-# sents=['this is a pen','this pen is my pen']
-sents = open("../../../nlptutorial/data/wiki-en-train.word").readlines()
+sents = open('./nlptutorial/data/wiki-en-train.word').readlines()
 d = {}
 for sent in sents:
-    for word in sent.strip().split(' '): 
+    for word in sent.split(' '): 
         if word not in d.keys():
             d[word] = 0
         d[word] += 1
-
-print(list(d.items())[:10])
+print (sorted(d.items()))
