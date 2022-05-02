80fc2e69faf0446ab880bc18b960da2daa2a9a0c
DUAN‐2022 <87805036+standalone2021@users.noreply.github.com>
2022-05-02 14:42:13 +0900

    Update train-bigram.py

--- duan/tutorial02/train-bigram.py
+++ duan/tutorial02/train-bigram.py
@@ -1,26 +1,28 @@
 import sys
 from collections import defaultdict
 
+#デフォルト値を設定する
 counts = defaultdict(lambda:0)
 context_counts = defaultdict(lambda:0)
-#　必要なファイルを読みこむ
+#ファイルの読み込み
 training_file = open('./nlptutorial/data/wiki-en-train.word').readlines()
 
 for line in training_file:
-    words = line.split()
-    words.append('</s>')
-    words.insert(0,'<s>')
+    words = line.split() #単語を分ける
+    words.append('</s>') #文末記号
+    words.insert(0,'<s>') #文頭記号
     for i in range(1,len(words)):
-        counts[words[i-1]+' '+words[i]] += 1
-        context_counts[words[i-1]] += 1
-        counts[words[i]] += 1
-        context_counts[''] += 1 
+        counts[words[i-1]+' '+words[i]] += 1 #2-gramの分子
+        context_counts[words[i-1]] += 1 #2-gramの分母
+        counts[words[i]] += 1 #1-gramの分子
+        context_counts[''] += 1  #1-gramの分母
 
+#ファイルの書き込み
 output = open('./nlptutorial/data/train-bigram.txt','w')
 
 for ngram, count in sorted(counts.items()):
-    context = ngram.split(' ')
-    context.pop()
-    context = ' '.join(context)
+    context = ngram.split(' ') #単語を分ける
+    context.pop() #末尾の要素を削除する
+    context = ' '.join(context) #文字列を連結する
     probability = float(counts[ngram]/context_counts[context])
-    output.write (ngram + '  ' + '{:.6f}'.format(probability) + '\n'
+    output.write (ngram + '  ' + '{:.6f}'.format(probability) + '\n') #文字列を書き込む
