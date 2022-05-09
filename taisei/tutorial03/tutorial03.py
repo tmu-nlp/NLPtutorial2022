b35c61cdffc46f509e63731c1e043f9d85e39dad
enomooon <enomoto-taisei@ed.tmu.ac.jp>
2022-05-09 11:41:42 +0900

    modify and add big

--- taisei/tutorial03/tutorial03.py
+++ taisei/tutorial03/tutorial03.py
@@ -1,5 +1,7 @@
 #python tutorial03.py ../../../nlptutorial/data/wiki-ja-train.word ../../../nlptutorial/data/wiki-ja-test.txt my_answer2.word
-#perl gradews.pl wiki-ja-test.word my_answer2.word > split_accuracy.txt
+#perl gradews.pl ../../../nlptutorial/data/wiki-ja-test.word my_answer2.word > split_accuracy.txt
+#python tutorial03.py ../../../nlptutorial/data/big-ws-model.txt ../../../nlptutorial/data/wiki-ja-test.txt my_answer_big.word
+#perl gradews.pl ../../../nlptutorial/data/wiki-ja-test.word my_answer_big.word > split_accuracy_big.txt
 from collections import defaultdict
 import math
 import sys
@@ -12,19 +14,25 @@ class SplitWord:
 
     
     def calcu_uni_prob(self, train_filename): #1-gramモデルの作成
-        cnt = 0
         with open(train_filename, "r") as f_train:
             data = f_train.readlines()
         for line in data:
             line = line.strip().split()
             line.append('</s>')
             for word in line:
-                cnt += 1
                 self.dict_uni_prob[word] += 1
-
+        cnt = sum(self.dict_uni_prob.values())
         for my_key, my_value in sorted(self.dict_uni_prob.items()):
             #self.dict_uni_prob[my_key] = my_value / sum(self.dict_uni_prob.values())
             self.dict_uni_prob[my_key] = my_value / cnt
+
+
+    def load_uni_prob(self, train_filename): #1-gramモデルの読み込み
+        with open(train_filename, "r") as f_train:
+            data = f_train.readlines()
+        for line in data:
+            line = line.strip().split("\t")
+            self.dict_uni_prob[line[0]] = float(line[1])
 
 
     def split_word(self, input_filename, out_filename): #前向きステップと後ろ向きステップの合体
@@ -32,13 +40,13 @@ class SplitWord:
             data = f_input.readlines()
         with open(out_filename, "w") as f_output:
             for line in data:
+                line = line.strip()
                 best_edge = self.front_step(line)
-                tar_str = self.back_step(line, best_edge)
-                f_output.write(f'{tar_str}\n')
+                ans_str = self.back_step(line, best_edge)
+                f_output.write(f'{ans_str}\n')
 
 
     def front_step(self, line): #前向きステップ
-        line = line.strip()
         best_score = [10000000] * (len(line) + 1)
         best_edge = [None] * (len(line) + 1)
         best_score[0] = 0
@@ -71,4 +79,5 @@ class SplitWord:
 if __name__ == "__main__":
     k = SplitWord()
     k.calcu_uni_prob(sys.argv[1])
+    #k.load_uni_prob(sys.argv[1]) #big-ws-model.txtは既にモデル学習済み
     k.split_word(sys.argv[2], sys.argv[3])
